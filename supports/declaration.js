9d68e86ec7514bee43b1b1f1e21d2dfb2251a3a9
Lea Verou <lea@verou.me>
2025-08-27 18:35:02 +0300

    [supports/declaration] Don't error for incorrect input

--- supports/declaration.js
+++ supports/declaration.js
@@ -3,6 +3,13 @@ import supportsVariable from './variable.js';
 
 export default function declaration (intruction) {
 	var val = intruction.match(/\s*([^:]+)\s*:\s*(.+)\s*/);
+	if (val === null) {
+		return {
+			success: false,
+			note: 'Invalid declaration'
+		}
+	}
+
 	return {
 		success: !val[1].match(/--.*/)
 			? supportsValue(val[1], val[2]).success
