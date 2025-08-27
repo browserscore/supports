7ebf6aa59f40d6fc8a67a1ca84abfe59405c3abe
Lea Verou <lea@verou.me>
2025-08-27 15:48:44 +0300

    Remove unnecessary imports

--- supports/mq.js
+++ supports/mq.js
@@ -1,6 +1,3 @@
-import { prefixes, dummy, inlineStyle } from './shared.js';
-import { camelCase, getPrefixedVariants } from './util.js';
-
 export default function mq (mq) {
 	return {
 		// We check whether the query does not include 'not all' because
