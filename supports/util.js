50aeedb7e6ae7b511724cb58c9c7cbd3937717d1
Lea Verou <lea@verou.me>
2025-08-27 18:32:30 +0300

    Refactor: `toArray()` util

--- supports/util.js
+++ supports/util.js
@@ -1,7 +1,15 @@
-export function camelCase(str) {
+export function camelCase (str) {
 	return str
 		.replace(/-([a-z])/g, function ($0, $1) {
 			return $1.toUpperCase();
 		})
 		.replace('-', '');
+}
+
+export function toArray (value) {
+	if (value === undefined || value === null) {
+		return [];
+	}
+
+	return Array.isArray(value) ? value : [value];
 }
