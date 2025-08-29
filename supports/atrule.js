32e29fd3da51a217b088ba811e713568f31282e9
Lea Verou <lea@verou.me>
2025-08-29 04:19:04 +0300

    Major: Overhaul atrules and descriptors
    - `CSSAtruleFeature` and friends, which can have children
    - Specify what's needed, no more specifying entire rules pointlessly with pointless descriptors inside them
    - Specify descriptors under their atrules. No more top-level descriptors type.
    - New descriptor detection function
    -

--- supports/atrule.js
+++ supports/atrule.js
@@ -1,30 +1,119 @@
 import { prefixes, styleElement } from './shared.js';
 
 let cached = {};
+let instances = {};
 
 /**
- * Low-level function to check if an at-rule is supported
- * @param {string} atrule - The at-rule to check as a string (e.g. "@supports (display: flex)")
+ * Low-level function to check if an at-rule is supported. No cache, no input fixup, no prefixing.
+ * @param {string} name - The at-rule to check as a string (e.g. "@supports (display: flex)"). Needs the @.
+ * @param {CSSRule | CSSStyleSheet} [parentRule] - Optionally a parent rule to insert the at-rule into.
  * @returns {boolean}
  */
-export function isSupported (atrule) {
-	styleElement.textContent = atrule; // Safari 4 has issues with style.innerHTML
-	return styleElement.sheet.cssRules.length > 0;
+export function isSupported (atrule, { parent, contentBefore = '' } = {}) {
+	let code = atrule;
+
+	if (!atrule.endsWith(';') && !atrule.endsWith('}')) {
+		code += `{ }`;
+	}
+
+	if (contentBefore) {
+		code = contentBefore + code;
+	}
+
+	if (parent) {
+		// Rules that are only valid inside other rules, e.g. @stylistic
+		let parentRule = parent.instance;
+
+		if (parentRule && parentRule.cssRules && (parentRule.insertRule || parentRule.appendRule)) {
+			if (parentRule.insertRule) {
+				// Most rules
+				parentRule.insertRule(code, 0);
+			}
+			else if (parentRule.appendRule) {
+				// E.g. CSSKeyframeRule
+				parentRule.appendRule(code);
+			}
+
+			return parentRule.cssRules[0];
+		}
+		else {
+			// Not an object we can use, fall back to using code :(
+			code = `${parent.resolved} { ${code} }`;
+		}
+	}
+
+	if (parent || contentBefore) {
+		let codeWithout = parent ? `${parent.resolved} { ${contentBefore} }` : contentBefore;
+
+		styleElement.textContent = codeWithout;
+		let obj = parent ? styleElement.sheet.cssRules[0] : styleElement.sheet.cssRules[0];
+		let cssTextWithout = obj.cssText;
+
+		styleElement.textContent = code;
+		let cssText = obj.cssText;
+
+		return cssText !== cssTextWithout;
+	}
+	else {
+		styleElement.textContent = code;
+		return styleElement.sheet.cssRules[0];
+	}
+
 }
 
-export default function (atrule) {
+/**
+ * Cached.
+ * @param {string} atrule An @-rule string including any prelude like e.g. "@supports (display: flex)", with or without the @
+ * @returns
+ */
+export default function supportsAtrule (atrule, {parent: parentRule, contentBefore} = {}) {
+	let parent;
+
+	if (parentRule) {
+		// Fail early if parent not supported
+		parent = supportsAtrule(parentRule);
+		if (!parent.success) {
+			return {success: false, parent: parent};
+		}
+	}
+
+	if (!atrule.startsWith('@')) {
+		atrule = "@" + atrule;
+	}
+	atrule = atrule.trim();
+
 	let cachedResult = cached[atrule];
-	let success, prefix;
+	let success, prefix, resolved;
 
 	if (cachedResult === undefined) {
-		prefix = prefixes.find(prefix => isSupported(atrule.replace(/^@/, '@' + prefix)));
-		success = prefix !== undefined;
+		for (let p of prefixes) {
+			resolved = prefixAtrule(atrule, p);
+			success = isSupported(resolved, {parent, contentBefore});
+
+			if (success) {
+				if (typeof success === 'object') {
+					instances[atrule] = success;
+				}
+
+				prefix = p;
+				success = true;
+				break;
+			}
+		}
+
 		cached[atrule] = prefix === '' ? true : (prefix ?? false);
 	}
 	else {
 		success = Boolean(cachedResult);
 		prefix = typeof cachedResult === "boolean" ? '' : cachedResult;
+		resolved = prefixAtrule(atrule, prefix);
 	}
 
-	return {success, prefix};
+	success ??= false;
+
+	return {success, prefix, instance: instances[atrule], resolved};
+}
+
+function prefixAtrule (atrule, prefix = '') {
+	return atrule.replace(/^@?/, '@' + prefix);
 }
--- supports/descriptorvalue.js
+++ supports/descriptorvalue.js
@@ -1,40 +1,53 @@
 import { styleElement } from './shared.js';
 import { camelCase } from './util.js';
 
-export default function descriptorValue (descriptor, value, required) {
-	/* doesn't handle prefixes for descriptor or value */
-	var add = '',
-		pos = 0;
-	if (descriptor.match(/@.*\//)) {
-		var part = descriptor.split('/');
-		var rule = part[0];
-		descriptor = part[1];
+import isDescriptorSupported from './descriptor.js';
+import {isSupported as isValueSupported} from './value.js';
 
-		if (required) {
-			if (required.rule) {
-				rule = required.rule + ' ' + rule;
-				pos = 1;
-			}
-			if (required.descriptor) {
-				add = required.descriptor + '; ';
-			}
-		}
-	} else {
-		var rule = '@font-face';
+/**
+ * Low-level function to check if an at-rule is supported
+ * @param {string} name - The descriptor to check as a string (e.g. "display")
+ * @param {string} value - The value to check as a string (defaults to "inherit")
+ * @param {string} atrule - The at-rule to check as a string (e.g. "@supports (display: flex)"). Must have @ already.
+ * @returns {boolean}
+ */
+export function isSupported (name, value, atrule) {
+	styleElement.textContent = `${atrule.resolved} { ${name}: ${value} ; }`;
+
+	let instance = styleElement.sheet.cssRules[0];
+	let style = instance.style ?? instance;
+
+	if (!style) {
+		return false;
+	}
+	let emptyValue = atrule.instance ? getValue(name, atrule.instance ?? atrule.instance.style) : '';
+	let serialized = getValue(name, style);
+
+	return serialized !== emptyValue;
+}
+
+function getValue (name, rule) {
+	if (rule.getPropertyValue) {
+		return rule.getPropertyValue(name);
 	}
 
-	styleElement.textContent = rule + ' {' + add + descriptor + ':' + value + '}';
-	try {
-		if (styleElement.sheet.cssRules.length) {
-			return {
-				success:
-					(styleElement.sheet.cssRules[pos].style && styleElement.sheet.cssRules[pos].style.length >= 1) ||
-					styleElement.sheet.cssRules[pos][camelCase(descriptor)] !== undefined,
-			};
-		} else {
-			return { success: false };
-		}
-	} catch (e) {
-		return { success: false };
+	return rule[camelCase(name)] || '';
+}
+
+/**
+ * No caching, no prefixes. Fails early if the descriptor or @rule are not supported at all.
+ * @param {string} name
+ * @param {string} value
+ * @param {string} atrule
+ * @returns
+ */
+export default function (name, value, atrule) {
+	let descriptor = isDescriptorSupported(name, atrule);
+
+	if (!descriptor.success) {
+		return {success: false, note: `Descriptor ${name} is not supported at all in ${descriptor.atrule.resolved}`};
 	}
+
+	let success = isSupported(name, value, descriptor.atrule);
+	return {success, descriptor};
 }
