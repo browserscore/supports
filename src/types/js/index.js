65b15cf9586524727289f1185b3a62828fddb2a4
Lea Verou <lea@verou.me>
2025-09-05 14:54:54 +0300

    Fix import

--- src/types/js/index.js
+++ src/types/js/index.js
@@ -1,3 +1,3 @@
-export { default as testExtends } from './testExtends.js';
+export { default as extends } from './extends.js';
 export { default as global } from './global.js';
 export { default as member } from './member.js';
