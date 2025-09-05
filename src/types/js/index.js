53d0ddcd7f97d46d107f4ed235b95f193227c1e7
Lea Verou <lea@verou.me>
2025-09-05 16:09:36 +0300

    Add event detection

--- src/types/js/index.js
+++ src/types/js/index.js
@@ -1,3 +1,4 @@
 export { default as extends } from './extends.js';
 export { default as global } from './global.js';
 export { default as member } from './member.js';
+export { default as event } from './event.js';
