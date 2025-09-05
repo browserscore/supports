d497d4dd98ee78aa71395e5356a3461dcad9684e
Lea Verou <lea@verou.me>
2025-09-05 18:10:32 +0300

    Supports syntax

--- src/types/js/index-fn.js
+++ src/types/js/index-fn.js
@@ -1,3 +1,4 @@
 export { default as jsMember } from './member.js';
 export { default as jsGlobal } from './global.js';
 export { default as jsEvent } from './event.js';
+export { default as jsSyntax } from './syntax.js';
