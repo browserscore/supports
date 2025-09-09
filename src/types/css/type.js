fa2ec297e31e4aebc088fcbb4efbbbec72d0466b
Lea Verou <lea@verou.me>
2025-09-09 18:57:11 +0300

    Add more types

--- src/types/css/type.js
+++ src/types/css/type.js
@@ -6,12 +6,13 @@ import supportsValue from './value.js';
 export const dataTypes = {
 	length: { property: 'width', sampleValue: '0px' },
 	percentage: { property: 'width', sampleValue: '0%' },
-	'length-percentage': { property: 'width', sampleValue: 'calc(1px + 1%)' },
+
 	time: { property: 'transition-duration', sampleValue: '0s' },
 	angle: { property: { name: 'transform', value: v => `rotate(${v})` }, sampleValue: '0deg' },
 	integer: { property: 'z-index', sampleValue: '0' },
 	number: { property: 'line-height', sampleValue: '0' },
-
+	frequency: { sampleValue: '0Hz' },
+	resolution: { property: 'image-resolution', sampleValue: '0dpi' },
 	image: {
 		property: 'background-image',
 		sampleValue:
@@ -21,7 +22,16 @@ export const dataTypes = {
 	'custom-ident': { property: 'animation-name', sampleValue: 'foo' },
 	'dashed-ident': { sampleValue: '--foo' },
 	color: { property: 'color', sampleValue: 'red' },
+
+	// https://drafts.csswg.org/css-values/#mixed-percentages
+	'length-percentage': { property: 'width', sampleValue: 'calc(1px + 1%)' },
+	'frequency-percentage': { sampleValue: 'calc(1Hz + 1%)' },
+	'angle-percentage': { sampleValue: 'calc(1deg + 1%)' },
+	'time-percentage': { sampleValue: 'calc(1s + 1%)' },
 };
+
+dataTypes.url = dataTypes.image;
+
 
 /**
  * Test whether a certain value is accepted as part of a given type
