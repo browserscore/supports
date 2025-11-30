3cbc7e1c5e7e8c6153dd22b78900eda4d82d04b7
Lea Verou <lea@verou.me>
2025-11-30 16:46:06 -0500

    Convert shared.js variables to named exports
    Changed dummy, inlineStyle, and styleElement to use named exports directly instead of exporting at the end of the file for improved clarity and consistency.

--- src/shared.js
+++ src/shared.js
@@ -1,8 +1,6 @@
-const dummy = document.createElement('_');
-const inlineStyle = dummy.style;
-const styleElement = document.createElement('style');
+export const dummy = document.createElement('_');
+export const inlineStyle = dummy.style;
+export const styleElement = document.createElement('style');
 
 document.documentElement.appendChild(styleElement);
 document.documentElement.appendChild(dummy);
-
-export {dummy, inlineStyle, styleElement}
