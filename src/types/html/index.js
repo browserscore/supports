6068daafa9eed0477b4a12b83580d02e740e6e8f
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-12-02 15:01:43 +0100

    Expose the new functions

--- src/types/html/index.js
+++ src/types/html/index.js
@@ -1,4 +1,3 @@
 export { default as element } from './element.js';
-// Uncomment when we know these work
-// export { default as attribute } from './attribute.js';
-// export { default as attributeValue } from './attribute-value.js';
+export { default as attribute } from './attribute.js';
+export { default as attributeValue } from './attribute-value.js';
